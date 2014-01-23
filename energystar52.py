609f86bdb7aa5521c509fb7ebb2885316e9d68e8
Shih-Yuan Lee (FourDollars) <sylee@canonical.com>
2014-01-23 15:35:30 +0800

    Refactor as EnergyStar52 class.

--- energystar52.py
+++ energystar52.py
@@ -35,193 +35,178 @@ def lineno():
     """Returns the current line number in our program."""
     return " (DEBUG: line %s)" % str(inspect.currentframe().f_back.f_lineno) 
 
-# Requirements for Desktop, Integrated Desktop, and Notebook Computers
-def equation_one(si):
-    """Equation 1: TEC Calculation (E_TEC) for Desktop, Integrated Desktop, and Notebook Computers"""
-    E_TEC = (8760 / 1000) * ((P_OFF * T_OFF) + (P_SLEEP * T_SLEEP) + (P_IDLE * T_IDLE))
+class EnergyStar52:
+    """Energy Star 5.2 calculator"""
+    def __init__(self, sysinfo):
+        self.sysinfo = sysinfo
 
-def equation_two(si):
-    """Equation 2: E_TEC_MAX Calculation for Desktop, Integrated Desktop
-    disk: the number of internal storage elements
-    gpu:  GPU Frame Buffer Width
-    """
+    # Requirements for Desktop, Integrated Desktop, and Notebook Computers
+    def equation_one(self):
+        """Equation 1: TEC Calculation (E_TEC) for Desktop, Integrated Desktop, and Notebook Computers"""
+        E_TEC = (8760 / 1000) * ((P_OFF * T_OFF) + (P_SLEEP * T_SLEEP) + (P_IDLE * T_IDLE))
+        return E_TEC
 
-    computer = question("""[1] Desktop
-[2] Integrated Desktop
-[3] Notebook
-Which type of computer do you use? [1-3] """, 1, "123")
+    def equation_two(self, computer, gpu_type, gpu_width):
+        """Equation 2: E_TEC_MAX Calculation for Desktop, Integrated Desktop
+        disk: the number of internal storage elements
+        gpu:  GPU Frame Buffer Width
+        """
 
-    core = si.get_cpu_info()
-    memory = si.get_memory_info()
-    category = ''
-    gpu_width = ''
-    gpu_type = ''
+        core = self.sysinfo.get_cpu_info()
+        memory = self.sysinfo.get_memory_info()
+        category = ''
 
-    ## Determine the category
-    # Desktop & Integrated Desktop
-    if computer == '1' or computer == '2':
-        if core >= 4:
-            if memory < 4:
-                gpu_width = question(GPUFrameBufferWidth % (128) + lineno(), 1, "yn")
-                if gpu_width == 'y':
+        ## Determine the category
+        # Desktop & Integrated Desktop
+        if computer == '1' or computer == '2':
+            if core >= 4:
+                if memory < 4:
+                    if gpu_width == 'y':
+                        category = 'D'
+                else:
                     category = 'D'
-            else:
-                category = 'D'
 
-        if not category and core >= 2:
-            if memory >= 2:
-                category = 'C'
-            else:
-                gpu_type = question(DiscreteGPU + lineno(), 1, "yn")
-                if gpu_type == 'y':
+            if not category and core >= 2:
+                if memory >= 2:
+                    category = 'C'
+                else:
+                    if gpu_type == 'y':
+                        category = 'C'
+
+            if not category and core == 2:
+                if memory >= 2:
+                    category = 'B'
+
+            if not category:
+                category = 'A'
+        # Notebook
+        else:
+            if core >= 2 and memory >= 2:
+                if gpu_width == 'y':
                     category = 'C'
 
-        if not category and core == 2:
-            if memory >= 2:
-                category = 'B'
+            if not category:
+                if gpu_type == 'y':
+                    category = 'B'
 
-        if not category:
-            category = 'A'
-    # Notebook
-    else:
-        if core >= 2 and memory >= 2:
-            gpu_width = question(GPUFrameBufferWidth % (64) + lineno(), 1, "yn")
-            if gpu_width == 'y':
-                category = 'C'
+            if not category:
+                category = 'A'
 
-        if not category:
-            gpu_type = question(DiscreteGPU + lineno(), 1, "yn")
-            if gpu_type == 'y':
-                category = 'B'
+        ## Maximum TEC Allowances for Desktop and Integrated Desktop Computers
+        if computer == '1' or computer == '2':
+            if category == 'A':
+                TEC_BASE = 148.0
 
-        if not category:
-            category = 'A'
+                if memory > 2:
+                    TEC_MEMORY = 1.0 * (memory - 2)
+                else:
+                    TEC_MEMORY = 0.0
 
-    print("Category %s" % (category))
+                if gpu_width == 'y':
+                    TEC_GRAPHICS = 50.0
+                else:
+                    TEC_GRAPHICS = 35.0
+            elif category == 'B':
+                TEC_BASE = 175.0
 
-    ## Maximum TEC Allowances for Desktop and Integrated Desktop Computers
-    if computer == '1' or computer == '2':
-        if category == 'A':
-            TEC_BASE = 148.0
+                if memory > 2:
+                    TEC_MEMORY = 1.0 * (memory - 2)
+                else:
+                    TEC_MEMORY = 0.0
 
-            if memory > 2:
-                TEC_MEMORY = 1.0 * (memory - 2)
+                if gpu_width == 'y':
+                    TEC_GRAPHICS = 50.0
+                else:
+                    TEC_GRAPHICS = 35.0
+            elif category == 'C':
+                TEC_BASE = 209.0
+
+                if memory > 2:
+                    TEC_MEMORY = 1.0 * (memory - 2)
+                else:
+                    TEC_MEMORY = 0.0
+
+                if gpu_width == 'y':
+                    TEC_GRAPHICS = 50.0
+                else:
+                    TEC_GRAPHICS = 0.0
+            elif category == 'D':
+                TEC_BASE = 234.0
+
+                if memory > 4:
+                    TEC_MEMORY = 1.0 * (memory - 4)
+                else:
+                    TEC_MEMORY = 0.0
+
+                if gpu_width == 'y':
+                    TEC_GRAPHICS = 50.0
+                else:
+                    TEC_GRAPHICS = 0.0
             else:
-                TEC_MEMORY = 0.0
+                raise Exception('This is a bug when you see this.')
 
-            if not gpu_width:
-                gpu_width = question(GPUFrameBufferWidth % (128) + lineno(), 1, "yn")
-            if gpu_width == 'y':
-                TEC_GRAPHICS = 50.0
+            disk = self.sysinfo.get_disk_info()
+            if disk > 1:
+                TEC_STORAGE = 25.0 * (disk - 1)
             else:
-                TEC_GRAPHICS = 35.0
-        elif category == 'B':
-            TEC_BASE = 175.0
-
-            if memory > 2:
-                TEC_MEMORY = 1.0 * (memory - 2)
-            else:
-                TEC_MEMORY = 0.0
-
-            if not gpu_width:
-                gpu_width = question(GPUFrameBufferWidth % (128) + lineno(), 1, "yn")
-            if gpu_width == 'y':
-                TEC_GRAPHICS = 50.0
-            else:
-                TEC_GRAPHICS = 35.0
-        elif category == 'C':
-            TEC_BASE = 209.0
-
-            if memory > 2:
-                TEC_MEMORY = 1.0 * (memory - 2)
-            else:
-                TEC_MEMORY = 0.0
-
-            if not gpu_width:
-                gpu_width = question(GPUFrameBufferWidth % (128) + lineno(), 1, "yn")
-            if gpu_width == 'y':
-                TEC_GRAPHICS = 50.0
-            else:
+                TEC_STORAGE = 0.0
+        ## Maximum TEC Allowances for Notebook Computers
+        else:
+            if category == 'A':
+                TEC_BASE = 40.0
                 TEC_GRAPHICS = 0.0
-        elif category == 'D':
-            TEC_BASE = 234.0
+            elif category == 'B':
+                TEC_BASE = 53.0
+                if gpu_width == 'y':
+                    TEC_GRAPHICS = 3.0
+                else:
+                    TEC_GRAPHICS = 0.0
+            elif category == 'C':
+                TEC_BASE = 88.5
+                TEC_GRAPHICS = 0.0
+            else:
+                raise Exception('This is a bug when you see this.')
 
             if memory > 4:
-                TEC_MEMORY = 1.0 * (memory - 4)
+                TEC_MEMORY = 0.4 * (memory - 4)
             else:
                 TEC_MEMORY = 0.0
 
-            if not gpu_width:
-                gpu_width = question(GPUFrameBufferWidth % (128) + lineno(), 1, "yn")
-            if gpu_width == 'y':
-                TEC_GRAPHICS = 50.0
+            disk = self.sysinfo.get_disk_info()
+            if disk > 1:
+                TEC_STORAGE = 3.0 * (disk - 1)
             else:
-                TEC_GRAPHICS = 0.0
-        else:
-            raise Exception('This is a bug when you see this.')
+                TEC_STORAGE = 0.0
 
-        disk = si.get_disk_info()
-        if disk > 1:
-            TEC_STORAGE = 25.0 * (disk - 1)
-        else:
-            TEC_STORAGE = 0.0
-    ## Maximum TEC Allowances for Notebook Computers
-    else:
-        if category == 'A':
-            TEC_BASE = 40.0
-            TEC_GRAPHICS = 0.0
-        elif category == 'B':
-            TEC_BASE = 53.0
-            if not gpu_width:
-                gpu_width = question(GPUFrameBufferWidth % (64) + lineno(), 1, "yn")
-            if gpu_width == 'y':
-                TEC_GRAPHICS = 3.0
-            else:
-                TEC_GRAPHICS = 0.0
-        elif category == 'C':
-            TEC_BASE = 88.5
-            TEC_GRAPHICS = 0.0
-        else:
-            raise Exception('This is a bug when you see this.')
+        E_TEC_MAX = TEC_BASE + TEC_MEMORY + TEC_GRAPHICS + TEC_STORAGE
 
-        if memory > 4:
-            TEC_MEMORY = 0.4 * (memory - 4)
-        else:
-            TEC_MEMORY = 0.0
+        return (category, E_TEC_MAX)
 
-        disk = si.get_disk_info()
-        if disk > 1:
-            TEC_STORAGE = 3.0 * (disk - 1)
-        else:
-            TEC_STORAGE = 0.0
-    E_TEC_MAX = TEC_BASE + TEC_MEMORY + TEC_GRAPHICS + TEC_STORAGE
-    return E_TEC_MAX
+    # Requirements for Workstations
+    def equation_three():
+        """Equation 3: P_TEC Calculation for Workstations""" 
+        P_TEC = (P_OFF * T_OFF) + (P_SLEEP * T_SLEEP) + (P_IDLE * T_IDLE) 
 
-# Requirements for Workstations
-def equation_three():
-    """Equation 3: P_TEC Calculation for Workstations""" 
-    P_TEC = (P_OFF * T_OFF) + (P_SLEEP * T_SLEEP) + (P_IDLE * T_IDLE) 
+    def equation_four():
+        """Equation 4: P_TEC_MAX Calculation for Workstations"""
+        P_TEC_MAX <= 0.28 * (P_MAX + (N_HDD * 5))
 
-def equation_four():
-    """Equation 4: P_TEC_MAX Calculation for Workstations"""
-    P_TEC_MAX <= 0.28 * (P_MAX + (N_HDD * 5))
+    # Requirements for Small-scale Servers
+    def equation_five():
+        """Equation 5: Calculation of P_OFF_MAX for Small-scale Servers"""
+        P_OFF_MAX = P_OFF_BASE + P_OFF_WOL
 
-# Requirements for Small-scale Servers
-def equation_five():
-    """Equation 5: Calculation of P_OFF_MAX for Small-scale Servers"""
-    P_OFF_MAX = P_OFF_BASE + P_OFF_WOL
+    # Requirements for Thin Clients
+    def equation_six():
+        """Equation 6: Calculation of P_OFF_MAX for Thin Clients"""
+        P_OFF_MAX = P_OFF_BASE + P_OFF_WOL
 
-# Requirements for Thin Clients
-def equation_six():
-    """Equation 6: Calculation of P_OFF_MAX for Thin Clients"""
-    P_OFF_MAX = P_OFF_BASE + P_OFF_WOL
-
-def equation_seven():
-    """Equation 7: Calculation of P_SLEEP_MAX for Thin Clients"""
-    P_SLEEP_MAX = P_SLEEP_BASE + P_SLEEP_WOL
+    def equation_seven():
+        """Equation 7: Calculation of P_SLEEP_MAX for Thin Clients"""
+        P_SLEEP_MAX = P_SLEEP_BASE + P_SLEEP_WOL
 
 def main():
-    si = SystemInformation()
+    sysinfo = SystemInformation()
 
     product_type = question("""[1] Desktop, Integrated Desktop, and Notebook Computers
 [2] Workstations (Not implemented yet)
@@ -230,9 +215,31 @@ def main():
 Which product type would you like to verify? [1-4] """, 1, "1234")
 
     if product_type == '1':
-        E_TEC_MAX = equation_two(si)
-        print("E_TEC_MAX = %s" % (E_TEC_MAX,))
-        E_TEC = equation_one(si)
+        computer_type = question("""[1] Desktop
+[2] Integrated Desktop
+[3] Notebook
+Which type of computer do you use? [1-3] """, 1, "123")
+
+        if computer_type == '3':
+            gpu_bit = '64'
+        else:
+            gpu_bit = '128'
+
+        estar52 = EnergyStar52(sysinfo)
+
+        discrete = question(DiscreteGPU, 1, "yn")
+
+        (cat1, max1) = estar52.equation_two(computer_type, discrete, 'y')
+        (cat2, max2) = estar52.equation_two(computer_type, discrete, 'n')
+
+        if cat1 == cat2 and max1 == max2:
+            print("Category %s: E_TEC_MAX = %s" % (cat1, max1))
+        else:
+            print("Category %s: E_TEC_MAX = %s (GPU Frame Buffer Width > %s-bit)" % (cat1, max1, gpu_bit))
+            print("Category %s: E_TEC_MAX = %s (GPU Frame Buffer Width <= %s-bit)" % (cat2, max2, gpu_bit))
+
+        E_TEC = estar52.equation_one(sysinfo)
+
         if E_TEC <= E_TEC_MAX:
             print("%s (E_TEC) <= %s (E_TEC_MAX) Passed." % (E_TEC, E_TEC_MAX))
         else:
