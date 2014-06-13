c8b3a01c157e074592bf5b499fa15b2c4f94c570
Shih-Yuan Lee (FourDollars) <sylee@canonical.com>
2014-06-13 17:24:41 +0800

    Implement the category of ErP Lot 3.

--- energy-tools.py
+++ energy-tools.py
@@ -21,6 +21,7 @@ import argparse
 import math
 import os
 import subprocess
+import unittest
 
 def debug(msg):
     if args.debug:
@@ -111,13 +112,7 @@ class SysInfo:
         self.ep = ep
         self.product_type = product_type
         self.computer_type = computer_type
-        if eee != -1:
-            self.eee = eee
-        else:
-            self.eee = 0
-            for eth in os.listdir("/sys/class/net/"):
-                if eth.startswith('eth') and subprocess.check_call('sudo ethtool %s | grep 1000 >/dev/null 2>&1' % eth, shell=True) == 0:
-                    self.eee = self.eee + 1
+        self.eee = eee
         self.tvtuner = tvtuner
         self.audio = audio
         self.off = off
@@ -136,6 +131,14 @@ class SysInfo:
 
         if auto:
             return
+
+        if eee != -1:
+            self.eee = eee
+        else:
+            self.eee = 0
+            for eth in os.listdir("/sys/class/net/"):
+                if eth.startswith('eth') and subprocess.check_call('sudo ethtool %s | grep 1000 >/dev/null 2>&1' % eth, shell=True) == 0:
+                    self.eee = self.eee + 1
 
         # Product type
         self.product_type = question_int("""Which product type would you like to verify?
@@ -1002,6 +1005,69 @@ def energystar_calculate(sysinfo):
                 print("    %s (E_TEC) %s %s (E_TEC_MAX), %s" % (E_TEC, operator, E_TEC_MAX, result))
     else:
         raise Exception('This is a bug when you see this.')
+
+class ErPLot3_2014:
+    """ErP Lot 3 calculator from 1 July 2014"""
+    def __init__(self, sysinfo):
+        self.computer_type = sysinfo.computer_type
+        self.cpu_core = sysinfo.cpu_core
+        self.cpu_clock = sysinfo.cpu_clock
+        self.memory_size = sysinfo.mem_size
+        self.discrete_graphics_cards = sysinfo.discrete_gpu_num
+        self.discrete_audio = sysinfo.audio
+        self.tv_tuner = sysinfo.tvtuner
+
+    def category(self, category):
+        if self.computer_type == 1 or self.computer_type == 2:
+            if category == 'D':
+                if self.cpu_core >= 4:
+                    if self.memory_size >= 4:
+                        return 1
+                    elif self.discrete_graphics_cards >= 1:
+                        return 0
+                    else:
+                        return -1
+                else:
+                    return -1
+            elif category == 'C':
+                if self.cpu_core >= 3:
+                    if self.memory_size >= 2 or self.discrete_graphics_cards >= 1:
+                        return 1
+                    else:
+                        return -1
+                else:
+                    return -1
+            elif category == 'B':
+                if self.cpu_core >= 2 and self.memory_size >= 2:
+                    return 1
+                else:
+                    return -1
+            elif category == 'A':
+                return 1
+            else:
+                raise Exception('Should not be here.')
+        elif self.computer_type == 3:
+            if category == 'C':
+                if self.cpu_core >= 2 and self.memory_size >= 2 and self.discrete_graphics_cards >= 1:
+                    return 0
+                else:
+                    return -1
+            elif category == 'B':
+                if self.discrete_graphics_cards >= 1:
+                    return 1
+                else:
+                    return -1
+            elif category == 'A':
+                return 1
+            else:
+                raise Exception('Should not be here.')
+        else:
+            raise Exception('Should not be here.')
+
+
+class ErPLot3_2016(ErPLot3_2014):
+    """ErP Lot 3 calculator from 1 January 2016"""
+    pass
 
 def erplot3_calculate(sysinfo):
     if sysinfo.product_type != 1:
@@ -2266,6 +2332,85 @@ def generate_excel_for_thin_clients(book, sysinfo, version):
     else:
         RESULT = "FAIL"
     sheet.write("E23", '=IF(E16<=E22, "PASS", "FAIL")', center, RESULT)
+
+class TestErPLot3(unittest.TestCase):
+    def test_desktop_category(self):
+        self.sysinfo = SysInfo(
+                auto=True,
+                computer_type=1,
+                cpu_core=4,
+                mem_size=4,
+                discrete_gpu_num=1)
+        inst = ErPLot3_2014(self.sysinfo)
+        self.assertEqual(inst.category('A'), 1)
+        self.assertEqual(inst.category('B'), 1)
+        self.assertEqual(inst.category('C'), 1)
+        self.assertEqual(inst.category('D'), 1)
+
+        self.sysinfo.mem_size = 2
+        inst = ErPLot3_2014(self.sysinfo)
+        self.assertEqual(inst.category('A'), 1)
+        self.assertEqual(inst.category('B'), 1)
+        self.assertEqual(inst.category('C'), 1)
+        self.assertEqual(inst.category('D'), 0)
+
+        self.sysinfo.discrete_gpu_num=0
+        inst = ErPLot3_2014(self.sysinfo)
+        self.assertEqual(inst.category('A'), 1)
+        self.assertEqual(inst.category('B'), 1)
+        self.assertEqual(inst.category('C'), 1)
+        self.assertEqual(inst.category('D'), -1)
+
+        self.sysinfo.discrete_gpu_num=1
+        self.sysinfo.mem_size = 1
+        inst = ErPLot3_2014(self.sysinfo)
+        self.assertEqual(inst.category('A'), 1)
+        self.assertEqual(inst.category('B'), -1)
+        self.assertEqual(inst.category('C'), 1)
+        self.assertEqual(inst.category('D'), 0)
+
+        self.sysinfo.cpu_core=2
+        inst = ErPLot3_2014(self.sysinfo)
+        self.assertEqual(inst.category('A'), 1)
+        self.assertEqual(inst.category('B'), -1)
+        self.assertEqual(inst.category('C'), -1)
+        self.assertEqual(inst.category('D'), -1)
+
+        self.sysinfo.mem_size = 2
+        inst = ErPLot3_2014(self.sysinfo)
+        self.assertEqual(inst.category('A'), 1)
+        self.assertEqual(inst.category('B'), 1)
+        self.assertEqual(inst.category('C'), -1)
+        self.assertEqual(inst.category('D'), -1)
+
+    def test_notebook_category(self):
+        self.sysinfo = SysInfo(
+                auto=True,
+                computer_type=3,
+                cpu_core=2,
+                mem_size=2,
+                discrete_gpu_num=1)
+        inst = ErPLot3_2014(self.sysinfo)
+        self.assertEqual(inst.category('A'), 1)
+        self.assertEqual(inst.category('B'), 1)
+        self.assertEqual(inst.category('C'), 0)
+        self.assertRaises(Exception, inst.category, 'D')
+
+        self.sysinfo.mem_size = 1
+        inst = ErPLot3_2014(self.sysinfo)
+        self.assertEqual(inst.category('A'), 1)
+        self.assertEqual(inst.category('B'), 1)
+        self.assertEqual(inst.category('C'), -1)
+
+        self.sysinfo.discrete_gpu_num=0
+        inst = ErPLot3_2014(self.sysinfo)
+        self.assertEqual(inst.category('A'), 1)
+        self.assertEqual(inst.category('B'), -1)
+        self.assertEqual(inst.category('C'), -1)
+
+    def tearDown(self):
+        #print(">>> Core: %d, Memory: %d, GPU: %d" % (self.sysinfo.cpu_core, self.sysinfo.mem_size, self.sysinfo.discrete_gpu_num))
+        pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
