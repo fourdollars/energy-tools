b80b501f3c77d7f335b4a79e741a5b9a80592e41
Shih-Yuan Lee (FourDollars) <sylee@canonical.com>
2014-06-16 14:09:27 +0800

    Calculate for ErP Lot 3 from 1 July 2014.

--- erplot3.py
+++ erplot3.py
@@ -27,9 +27,15 @@ class ErPLot3_2014:
         self.cpu_core = sysinfo.cpu_core
         self.cpu_clock = sysinfo.cpu_clock
         self.memory_size = sysinfo.mem_size
+        self.disk_number = sysinfo.disk_num
         self.discrete_graphics_cards = sysinfo.discrete_gpu_num
         self.discrete_audio = sysinfo.audio
         self.tv_tuner = sysinfo.tvtuner
+        self.off = sysinfo.off
+        self.off_wol = sysinfo.off_wol
+        self.sleep = sysinfo.sleep
+        self.sleep_wol = sysinfo.sleep_wol
+        self.idle = sysinfo.short_idle
 
     def category(self, category):
         if self.computer_type == 1 or self.computer_type == 2:
@@ -78,6 +84,88 @@ class ErPLot3_2014:
         else:
             raise Exception('Should not be here.')
 
+    def get_E_TEC(self):
+        if self.computer_type == 3:
+            (T_OFF, T_SLEEP, T_IDLE) = (0.6, 0.1, 0.3)
+        else:
+            (T_OFF, T_SLEEP, T_IDLE) = (0.55, 0.05, 0.4)
+
+        E_TEC = ((T_OFF * self.off) + (T_SLEEP * self.sleep) + (T_IDLE * self.idle)) * 8760 / 1000
+        return E_TEC
+
+    def get_E_TEC_wol(self):
+        if self.computer_type == 3:
+            (T_OFF, T_SLEEP, T_IDLE) = (0.6, 0.1, 0.3)
+        else:
+            (T_OFF, T_SLEEP, T_IDLE) = (0.55, 0.05, 0.4)
+
+        E_TEC = ((T_OFF * self.off_wol) + (T_SLEEP * self.sleep_wol) + (T_IDLE * self.idle)) * 8760 / 1000
+        return E_TEC
+
+    def get_TEC_BASE(self, category):
+        # Notebook
+        if self.computer_type == 3:
+            if category == 'A':
+                return 36
+            elif category == 'B':
+                return 48
+            elif category == 'C':
+                return 80.5
+        # Desktop
+        else:
+            if category == 'A':
+                return 133
+            elif category == 'B':
+                return 158
+            elif category == 'C':
+                return 188
+            elif category == 'D':
+                return 211
+        raise Exception("Should not be here.")
+
+    def get_TEC_TV_TUNER(self):
+        if self.computer_type == 3:
+            if self.tv_tuner:
+                return 2.1
+            else:
+                return 0
+        else:
+            if self.tv_tuner:
+                return 15
+            else:
+                return 0
+
+    def get_TEC_AUDIO(self):
+        if self.computer_type == 3:
+            return 0
+        else:
+            if self.audio:
+                return 15
+            else:
+                return 0
+
+    def get_MEMORY(self, category):
+        # Notebook
+        if self.computer_type == 3:
+            if self.memory_size > 4:
+                return 0.4 * (self.memory_size - 4)
+            else:
+                return 0
+        # Desktop
+        else:
+            if category == 'D':
+                return 1.0 * (self.memory_size - 4)
+            else:
+                if self.memory_size > 2:
+                    return 1.0 * (self.memory_size - 2)
+                else:
+                    return 0
+
+    def get_TEC_STORAGE(self):
+        if self.computer_type == 3:
+            return 3 * (self.disk_number - 1)
+        else:
+            return 25 * (self.disk_number - 1)
 
 class ErPLot3_2016(ErPLot3_2014):
     """ErP Lot 3 calculator from 1 January 2016"""
