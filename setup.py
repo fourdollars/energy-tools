1cff40b250087caf4f0850e431d8ffe8d0727d59
Shih-Yuan Lee (FourDollars) <sylee@canonical.com>
2018-07-05 14:59:28 +0800

    Bump to 1.4.3.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from distutils.core import setup
 import glob, os
 
 setup(name='energy-tools',
-      version='1.4.1',
+      version='1.4.3',
       description='Energy Tools for Energy Star (5.2 & 6.0 & 7.0) and ErP Lot 3 (Jul. 2014 & Jan. 2016)',
       long_description='This program is designed to collect the system profile and calculate\nthe results of Energy Star (5.2 & 6.0 & 7.0) and ErP Lot 3 (Jul. 2014 & Jan. 2016).',
       platforms=['Linux'],
