97a67a673378760aa397cdce00e3e57ba0305be4
Shih-Yuan Lee (FourDollars) <sylee@canonical.com>
2019-09-17 10:52:10 +0800

    Remove ErP Lot 3 Jul. 2014.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(name='energy-tools',
       long_description='''
 This program is designed to collect the system profile and calculate
 the results of Energy Star (5.2 & 6.0 & 7.0 & 8.0) and
-ErP Lot 3 (Jul. 2014 & Jan. 2016).''',
+ErP Lot 3 (Jan. 2016).''',
       platforms=['Linux'],
       license='GPLv3',
       author='Shih-Yuan Lee (FourDollars)',
